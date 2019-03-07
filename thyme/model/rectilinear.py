fe5cc3c0a016c4abeb9cbdb8120eea1a5cd35916
erin.nagel <Erin.Nagel@gmail.com>
2019-03-07 12:12:16 -0500

    Add module to process rectilinear grids

---
+++ thyme/model/rectilinear.py
@@ -0,0 +1,248 @@
+"""
+Utility classes and methods for working with a rectilinear grid output.
+
+This module provides functionality allowing model output to be interpolated to
+a regular, orthogonal lat/lon horizontal grid at a given depth-below-surface or
+to be optimized for encoding in a different format.
+
+"""
+import datetime
+
+import netCDF4
+import numpy
+from osgeo import ogr
+
+from thyme.model import model
+
+# Default fill value for NetCDF variables
+FILLVALUE = -9999.0
+
+# Default module for horizontal interpolation
+INTERP_METHOD_SCIPY = 'scipy'
+
+# Alternative module for horizontal interpolation
+INTERP_METHOD_GDAL = 'gdal'
+
+
+class RectilinearIndexFile(model.ModelIndexFile):
+    """NetCDF file containing metadata/grid info used during processing.
+
+    Store a regular grid definition, mask, and other information needed to
+    process/convert native output from a hydrodynamic model,
+    within a reusable NetCDF file.
+
+    Support is included for defining a set of regular, orthogonal subgrids that
+    allow the data to be subset into multiple sub-domains during processing.
+    This is accomplished by specifying a polygon shapefile containing one or
+    more rectangular, orthogonal polygons defining areas where output data will
+    be cropped and written to distinct output files.
+
+    A unique model index file must be created for each combination of model,
+    output grid resolution, land mask, and subset grid definition, and must be
+    regenerated if anything changes (i.e., when a model domain extent is
+    modified or the target output grid is redefined, a new model index file
+    must be created before processing can resume). Until any of these
+    properties change, the index file may be kept on the data processing system
+    and reused in perpetuity.
+    """
+
+    def __init__(self, path):
+        super().__init__(path)
+
+    def compute_grid_mask(self, model_file, reg_grid):
+        """Create model domain mask and write to index file.
+
+        Args:
+            model_file: `RECTILINEAROutputFile` instance containing rectlinear
+                grid structure and variables.
+            reg_grid: `RegularGrid` instance describing the regular grid for
+                which the mask will be created.
+        """
+        # Create OGR layer in memory
+        driver = ogr.GetDriverByName('Memory')
+        dset = driver.CreateDataSource('grid_cell_mask')
+        dset_srs = ogr.osr.SpatialReference()
+        dset_srs.ImportFromEPSG(4326)
+        layer = dset.CreateLayer('', dset_srs, ogr.wkbMultiPolygon)
+        layer.CreateField(ogr.FieldDefn('id', ogr.OFTInteger))
+
+        # Create polygons for each rectilinear grid cell
+        # searching counter clockwise(eta1, xi), (eta2, xi2), (eta3, xi3),(eta4, xi4)
+        for nx1 in range(model_file.num_x-1):
+            for ny1 in range(model_file.num_y-1):
+                if nx1 == 0:
+                    continue
+                if ny1 == 0:
+                    continue
+                if ny1 == model_file.num_y-1:
+                    continue
+                if nx1 == model_file.num_x-1:
+                    continue
+                nx2 = nx1 + 1
+                ny2 = ny1
+                nx3 = nx1 + 1
+                ny3 = ny1 + 1
+                nx4 = nx1
+                ny4 = ny1 + 1
+
+                # Search valid points
+                valid_points = []
+                for (ny, nx) in ((ny1, nx1), (ny2, nx2), (ny3, nx3), (ny4, nx4)):
+                    if model_file.var_u.mask[0, 0, ny, nx]:
+                        continue
+                    valid_points.append((ny, nx))
+                if len(valid_points) < 3:
+                    continue
+                ring = ogr.Geometry(ogr.wkbLinearRing)
+                for (ny, nx) in valid_points:
+                    ring.AddPoint(model_file.var_lon[ny, nx], model_file.var_lat[ny, nx])
+                (ny, nx) = valid_points[0]
+                ring.AddPoint(model_file.var_lon[ny, nx], model_file.var_lat[ny, nx])
+
+                # Create polygon
+                geom = ogr.Geometry(ogr.wkbPolygon)
+                geom.AddGeometry(ring)
+                feat = ogr.Feature(layer.GetLayerDefn())
+                feat.SetField('id', 1)
+                feat.SetGeometry(geom)
+                layer.CreateFeature(feat)
+
+        return self.rasterize_mask(reg_grid, layer)
+
+
+class RectilinearFile(model.ModelFile):
+    """Read/process data from a rectilinear model NetCDF file.
+
+    Attributes:
+        path: Path (relative or absolute) of the file.
+    """
+
+    def __init__(self, path):
+        """Initialize rectilinear file object and open file at specified path.
+
+        Args:
+            path: Path of target NetCDF file.
+
+        """
+        super().__init__(path)
+        self.var_lat = None
+        self.var_lon = None
+        self.var_u = None
+        self.var_v = None
+        self.var_date = None
+        self.var_x = None
+        self.var_y = None
+        self.var_depth = None
+        self.var_time = None
+        self.time_units = None
+        self.var_datetime = None
+        self.datetime_values = None
+        self.var_mask = None
+        self.num_x = None
+        self.num_y = None
+        self.num_times = None
+
+    def close(self):
+        super().close()
+        self.release_resources()
+
+    def release_resources(self):
+        self.var_lat = None
+        self.var_lon = None
+        self.var_u = None
+        self.var_v = None
+        self.var_date = None
+        self.var_x = None
+        self.var_y = None
+        self.var_depth = None
+        self.var_time = None
+        self.time_units = None
+        self.var_datetime = None
+        self.datetime_values = None
+        self.var_mask = None
+        self.num_x = None
+        self.num_y = None
+        self.num_times = None
+
+    def get_valid_extent(self):
+        """Masked model domain extent."""
+        lon_min = numpy.nanmin(self.var_lon)
+        lon_max = numpy.nanmax(self.var_lon)
+        lat_min = numpy.nanmin(self.var_lat)
+        lat_max = numpy.nanmax(self.var_lat)
+
+        return lon_min, lon_max, lat_min, lat_max
+
+    def init_handles(self):
+        """Initialize handles to NetCDF variables."""
+        self.var_lat = self.nc_file.variables['lat'][:, :]
+        self.var_lon = self.nc_file.variables['lon'][:, :]
+        self.var_lat = self.var_lat.astype(numpy.float64)
+        self.var_lon = self.var_lon.astype(numpy.float64)
+        self.var_u = self.nc_file.variables['u'][:, :, :, :]
+        self.var_v = self.nc_file.variables['v'][:, :, , :]
+        self.var_depth = self.nc_file.variables['depth'][:]
+        self.var_time = self.nc_file.variables['time'][:]
+        self.time_units = self.nc_file.variables['time'].units
+        self.num_times = self.nc_file.dimensions['time'].size
+        self.num_x = self.nc_file.dimensions['X'].size
+        self.num_y = self.nc_file.dimensions['Y'].size
+
+        # Use the surface layer in the u variable to define a land mask
+        mask_u = self.nc_file.variables['u'][0, 0, :, :]
+        mask_v = self.nc_file.variables['v'][0, 0, :, :]
+        self.var_mask = numpy.ma.mask_or(mask_u.mask, mask_v.mask)
+
+        # Convert timestamps to datetime objects and store in a list
+        # Rounding to the nearest hour
+        self.datetime_values = []
+        for time_index in range(self.num_times):
+            self.var_datetime = netCDF4.num2date(self.var_time, units=self.time_units)[time_index]
+            if self.var_datetime.minute >= 30:
+                # round up
+                adjusted_time = datetime.datetime(self.var_datetime.year, self.var_datetime.month, self.var_datetime.day, self.var_datetime.hour, 0, 0) + datetime.timedelta(hours=1)
+            elif self.var_datetime.minute < 30:
+                # round down
+                adjusted_time = datetime.datetime(self.var_datetime.year, self.var_datetime.month, self.var_datetime.day, self.var_datetime.hour, 0, 0)
+
+            self.datetime_values.append(adjusted_time)
+
+    def uv_to_regular_grid(self, model_index, time_index, target_depth, interp=INTERP_METHOD_SCIPY):
+        """Call grid processing functions and interpolate u/v to a regular grid"""
+
+        u_compressed, v_compressed, lat_compressed, lon_compressed = compress_variables(self.var_u, self.var_v, self.var_lat, self.var_lon, self.var_mask, time_index)
+
+        # Scipy interpolation is default method, change method parameter to change interpolation method
+        if interp == model.INTERP_METHOD_SCIPY:
+            return model.scipy_interpolate_uv_to_regular_grid(u_compressed, v_compressed, lat_compressed, lon_compressed, model_index)
+        elif interp == model.INTERP_METHOD_GDAL:
+            return model.gdal_interpolate_uv_to_regular_grid(u_compressed, v_compressed, lat_compressed, lon_compressed, model_index)
+
+
+def compress_variables(u, v, lat, lon, mask, time_index):
+    """Compress masked variables for interpolation.
+
+    Args:
+        u: `numpy.ndarray` containing u values for entire grid.
+        v: `numpy.ndarray` containing v values for entire grid.
+        lat: `numpy.ma.masked_array` containing latitude values.
+        lon: `numpy.ma.masked_array` containing longitude values.
+        mask: `numpy.ma.masked_array` containing u/v mask values.
+        time_index: Single forecast time index value.
+    """
+
+    u_single = u[time_index, 0, :, :]
+    v_single = v[time_index, 0, :, :]
+
+    water_lat_rho = numpy.ma.masked_array(lat, mask)
+    water_lon_rho = numpy.ma.masked_array(lon, mask)
+    water_u = numpy.ma.masked_array(u_single, mask)
+    water_v = numpy.ma.masked_array(v_single, mask)
+
+    u_compressed = numpy.ma.compressed(water_u)
+    v_compressed = numpy.ma.compressed(water_v)
+    lat_compressed = numpy.ma.compressed(water_lat_rho)
+    lon_compressed = numpy.ma.compressed(water_lon_rho)
+
+    return u_compressed, v_compressed, lat_compressed, lon_compressed
+
