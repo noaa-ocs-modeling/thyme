dbd64c552876509b0915633ba3833c7d5dd1ebcb
Erin Nagel <Erin.Nagel@noaa.gov>
2019-05-23 15:04:43 -0400

    Update package version in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(HERE, 'README.rst'), encoding='utf-8') as f:
 
 setuptools.setup(
     name='thyme',
-    version='0.1.0',
+    version='0.2.1',
     license='',
     author='Erin Nagel, Jason Greenlaw',
     author_email='erin.nagel@noaa.gov, jason.greenlaw@noaa.gov',
