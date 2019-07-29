c28bba71dd03616d2a3fa8db1b759660ab08b2e4
erin-nagel <Erin.Nagel@noaa.gov>
2019-07-29 16:28:16 -0400

    Add project url and license to setup.py

--- setup.py
+++ setup.py
@@ -6,25 +6,25 @@ import os
 HERE = pathlib.Path(__file__).parent
 
 # Get the long description from the README file
-with open(os.path.join(HERE, 'README.rst'), encoding='utf-8') as f:
+with open(os.path.join(HERE, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setuptools.setup(
     name='thyme',
-    version='0.2.1',
-    license='',
+    version='0.3.0',
+    license='BSD-2-Clause',
     author='Erin Nagel, Jason Greenlaw',
     author_email='erin.nagel@noaa.gov, jason.greenlaw@noaa.gov',
     description='Tools for Hydrodynamic Model Output Extraction and Processing',
     long_description=long_description,
-    url='',
+    url='https://github.com/noaa-ocs-modeling/thyme',
     packages=setuptools.find_packages(),
     setup_requires=['numpy'],
     install_requires=['shapely', 'numpy', 'scipy', 'netCDF4', 'gdal'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved',
+        'License :: OSI Approved :: BSD 2-Clause "Simplified License" (BSD-2-Clause)',
         'Operating System :: OS Independent',
     ],
 )
