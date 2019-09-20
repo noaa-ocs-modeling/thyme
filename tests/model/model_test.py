da2602103566716c587ff9abaabd323b412f6495
erin-nagel <Erin.Nagel@noaa.gov>
2019-09-20 11:10:32 -0400

    Add irregular u/v to spd/dir test for model module

--- tests/model/model_test.py
+++ tests/model/model_test.py
@@ -4,6 +4,7 @@ import numpy
 import pytest
 
 from thyme.model.model import regular_uv_to_speed_direction
+from thyme.model.model import irregular_uv_to_speed_direction
 
 RegGridValues = namedtuple(
     'RegGridValues',
@@ -12,6 +13,7 @@ RegGridValues = namedtuple(
      'expected_speed',
      'expected_direction'])
 
+
 @pytest.fixture
 def reg_grid_values():
     u = numpy.ma.masked_array(
@@ -19,7 +21,7 @@ def reg_grid_values():
             [0, 0.3, 0.5],
             [0.3, 0.5, 0.7],
             [999, 999, 999]
-        ], mask = [
+        ], mask=[
             [0, 0, 0],
             [0, 0, 0],
             [1, 1, 1]
@@ -30,7 +32,7 @@ def reg_grid_values():
             [0, 0.1, 0.2],
             [0.4, 0.3, 999],
             [999, 999, 0.7]
-        ], mask = [
+        ], mask=[
             [0, 0, 0],
             [0, 0, 1],
             [1, 1, 0]
@@ -67,3 +69,87 @@ def test_regular_uv_to_speed_direction(reg_grid_values):
     assert numpy.allclose(speed, reg_grid_values.expected_speed)
     assert numpy.allclose(direction, reg_grid_values.expected_direction)
 
+
+IrregularGridValues = namedtuple(
+    'RegGridValues',
+    ['u',
+     'v',
+     'expected_speed',
+     'expected_direction'])
+
+
+@pytest.fixture
+def irregular_grid_values():
+    u = numpy.ma.masked_array(
+        [
+            [-0.145427],
+            [-0.148755],
+            [-0.150399],
+            [-0.154676],
+            [-0.119714],
+            [999]
+        ], mask=[
+            [0],
+            [0],
+            [0],
+            [0],
+            [0],
+            [1]
+        ])
+
+    v = numpy.ma.masked_array(
+        [
+            [999],
+            [0.039037],
+            [0.019768],
+            [0.008497],
+            [0.026284],
+            [0.040731]
+        ], mask=[
+            [1],
+            [0],
+            [0],
+            [0],
+            [0],
+            [0]
+        ])
+
+    combined_mask = [
+            [1],
+            [0],
+            [0],
+            [0],
+            [0],
+            [1]
+        ]
+
+    expected_speed = numpy.ma.masked_array(
+        [
+            [999],
+            [0.29894739389419556],
+            [0.294866681098938],
+            [0.301119327545166],
+            [0.23824812471866608],
+            [999]
+        ], mask=combined_mask)
+
+    expected_direction = numpy.ma.masked_array(
+        [
+            [999],
+            [284.70428466796875],
+            [277.48785400390625],
+            [273.14434814453125],
+            [282.3831787109375],
+            [999]
+        ], mask=combined_mask)
+
+    return IrregularGridValues(u, v, expected_speed, expected_direction)
+
+
+def test_irregular_uv_to_speed_direction(irregular_grid_values):
+    """Test conversion from u/v (m/s) to speed/direction (knots/degrees)"""
+    speed, direction = irregular_uv_to_speed_direction(irregular_grid_values.u, irregular_grid_values.v)
+    print(f"speed: {speed}")
+    print(f"direction: {direction}")
+    assert numpy.allclose(speed, irregular_grid_values.expected_speed)
+    assert numpy.allclose(direction, irregular_grid_values.expected_direction)
