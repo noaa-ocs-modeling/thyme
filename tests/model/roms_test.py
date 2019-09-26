4ab624f7d52bdcae6c502c8364e0d1782c626937
erin-nagel <Erin.Nagel@noaa.gov>
2019-09-26 11:02:07 -0400

    Add test for roms module rotate_uv2d function

--- tests/model/roms_test.py
+++ tests/model/roms_test.py
@@ -71,3 +71,61 @@ def test_average_uv2rho(uv_average_values):
     print(f"v_rho: {v_rho}")
     assert numpy.allclose(u_rho, uv_average_values.expected_averaged_u)
     assert numpy.allclose(v_rho, uv_average_values.expected_averaged_v)
+
+
+UVRotateValues = namedtuple(
+    'UVRotateValues',
+    ['u_rho',
+     'v_rho',
+     'ang_rho',
+     'expected_rotated_u',
+     'expected_rotated_v'])
+
+@pytest.fixture
+def uv_rotate_values():
+    u_rho = numpy.array(
+        [
+            [-0.62173835, -0.58608955, -0.5823435],
+            [-0.74930345, -0.7648328, -0.7612157],
+            [-0.67852425, -0.6816753, -0.6829212]
+        ])
+
+    v_rho = numpy.array(
+        [
+            [-0.04093679, -0.04240224, -0.03317075],
+            [-0.00551716, -0.00082784, 0.01110598],
+            [0.05146192, 0.0615576,  0.06413107]
+        ])
+    ang_rho = numpy.array(
+        [
+            [-0.312156994200958, -0.311980178561144, -0.311803272848723],
+            [-0.311626277038169, -0.311449191104021, -0.311272015022893],
+            [-0.31109474877147, -0.310917392324487, -0.310739945656742]
+        ])
+
+    expected_rotated_u = numpy.array(
+        [
+            [-0.604264, -0.57081273, -0.56443999],
+            [-0.71490575, -0.72829074, -0.72123387],
+            [-0.63020189, -0.63015878, -0.63060553]
+
+        ])
+
+    expected_rotated_v = numpy.array(
+        [
+            [0.15198492, 0.13954119, 0.14707737],
+            [0.22449024, 0.23358615, 0.24370964],
+            [0.25668866, 0.26715252, 0.26987187]
+
+        ])
+
+    return UVRotateValues(u_rho, v_rho, ang_rho, expected_rotated_u, expected_rotated_v)
+
+
+def test_rotate_uv2d(uv_rotate_values):
+    """Test rotating u/v values by geometric angle"""
+    rot_u_rho, rot_v_rho = rotate_uv2d(uv_rotate_values.u_rho, uv_rotate_values.v_rho, uv_rotate_values.ang_rho)
+    print(f"rot_u: {rot_u_rho}")
+    print(f"rot_v: {rot_v_rho}")
+    assert numpy.allclose(rot_u_rho, uv_rotate_values.expected_rotated_u)
+    assert numpy.allclose(rot_v_rho, uv_rotate_values.expected_rotated_v)
