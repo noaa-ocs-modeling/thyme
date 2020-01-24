43d0915132d2bad373d9cd2321f3d10ea7b07925
erin-nagel <Erin.Nagel@noaa.gov>
2020-01-24 17:01:21 -0500

    Remove setuptools_scm to set package version from tagged commits

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ with open(os.path.join(HERE, 'README.md'), encoding='utf-8') as f:
 setuptools.setup(
     name='thyme',
     license='BSD-2-Clause',
+    version='0.4.5',
     author='Erin Nagel, Jason Greenlaw',
     author_email='erin.nagel@noaa.gov, jason.greenlaw@noaa.gov',
     description='Tools for Hydrodynamic Model Output Extraction and Processing',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/noaa-ocs-modeling/thyme',
-    use_scm_version=True,
-    setup_requires=['numpy', 'setuptools_scm'],
+    setup_requires=['numpy'],
     install_requires=['shapely', 'numpy', 'scipy', 'netCDF4', 'gdal'],
     classifiers=[
         'Programming Language :: Python :: 3',
